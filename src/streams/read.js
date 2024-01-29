26f720e6539c84bfb226cb693aa1d12b82e46ea8
vlad3k <vladislav.poezzhaev@gmail.com>
2024-01-29 22:02:45 +0700

    feat: implement task Streams - read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,13 @@
+import { promises as fsPromises } from 'node:fs';
+import process from 'node:process';
+
 const read = async () => {
-    // Write your code here 
+  const path = './files/fileToRead.txt';
+  const stream = (await fsPromises.open(path)).createReadStream();
+  stream.pipe(process.stdout);
+  stream.on('end', () => {
+    process.stdout.write('\n');
+  });
 };
 
 await read();
