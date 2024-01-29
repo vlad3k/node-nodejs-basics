4b76441b6459dca3ae6aee4be7cd650eb7a9de6e
vlad3k <vladislav.poezzhaev@gmail.com>
2024-01-29 23:23:41 +0700

    feat: implement task Streams - write.js

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,18 @@
+import fs from 'node:fs';
+import { createInterface } from 'readline';
+
 const write = async () => {
-    // Write your code here 
+  const path = './files/fileToWrite.txt'
+  const stream = fs.createWriteStream(path);
+  const rl = createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+
+  rl.question('', (userInput) => {
+    stream.write(userInput);
+    rl.close();
+  });
 };
 
 await write();
