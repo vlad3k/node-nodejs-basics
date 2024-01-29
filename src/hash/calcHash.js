41abb515b0e770f49cd540d9b46b9f144d3d36d3
vlad3k <vladislav.poezzhaev@gmail.com>
2024-01-29 19:17:42 +0700

    feat: implement task Hash - calcHash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,18 @@
+import fs from 'node:fs';
+import crypto from 'node:crypto';
+
 const calculateHash = async () => {
-    // Write your code here 
+  const path = './files/fileToCalculateHashFor.txt';
+  const hash = crypto.createHash('sha256');
+  const stream = fs.createReadStream(path);
+
+  stream.on('data', (data) => {
+    hash.update(data);
+  });
+
+  stream.on('end', () => {
+    console.log(hash.digest('hex'));
+  });
 };
 
 await calculateHash();
